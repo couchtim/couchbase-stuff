5820c077e59e2d9b6f52857b0bb96c811eefdaed
Tim Smith <tim@couchbase.com>
2012-09-29 23:13:29 -0700

    dump-keys: fix no-filter case, parallel queries
    Fix a bug in the match logic that printed nothing when no
    filter was specified.
    
    Require the -a option to dump all keys. Since this is intended
    for piping to a delete script, it seems safer to require a
    explicit option for this.
    
    Run 4 subshells in parallel for reading the vbucket tables, one
    for each bucket_name-[0-3].mb sqlite file.
    
    Other minor cleanups.

--- dump-keys-sqlite3.sh
+++ dump-keys-sqlite3.sh
@@ -4,16 +4,18 @@ note () { echo "$@" >&2; }
 die () { note "FATAL ERROR:" "$@"; exit 2; }
 
 usage () {
+    [ $# -gt 0 ] && echo "$@" >&2
     cat <<EOF
 Usage: $0 [options] /path/to/bucket-data/bucket
 
 Options:
+  -a         Dump all keys (no filter)
   -f REGEX   Filter the list of keys on (egrep) REGEX
 
 Examples:
 
 Print all keys in default bucket:
-  $0 /opt/couchbase/var/lib/couchbase/data/default-data/default
+  $0 -a /opt/couchbase/var/lib/couchbase/data/default-data/default
 
 Print keys starting with 'foo' in 'example' bucket:
   $0 -f '^foo' /opt/couchbase/var/lib/couchbase/data/example-data/example
@@ -23,14 +25,21 @@ EOF
 sqlite=/opt/couchbase/bin/sqlite3
 [ -x "$sqlite" ] || die "Can't find sqlite command-line executable at '$sqlite'"
 
+allkeys=0
 filter=
-while getopts df: name
+while getopts af: name
 do
     case $name in
+        a)  allkeys=1 ;;
         f)  filter=$OPTARG ;;
         ?)  usage; exit 1 ;;
     esac
 done
+
+if [ $allkeys -eq 1 -a -n "$filter" -o $allkeys -eq 0 -a -z "$filter" ]; then
+    usage "One of -a or -f REGEX must be specified" >&2
+    exit 1
+fi
 
 shift $(($OPTIND - 1))
 
@@ -49,29 +58,40 @@ activeCount=$(echo "$active" | wc -l)
 [ $activeCount -gt 0 ] || die "No active vbuckets for $bucket on this host"
 note "Reading keys from $activeCount active vbuckets"
 
-seenVbuckets=0
-seenItems=1
+seenItems=0
 matchedItems=0
 
-for part in 0 1 2 3; do
-    # Process each bucket-{0,1,2,3}.mb file in turn, to
-    # take more advantage of filesystem buffer
-    for vbid in $active; do
-        [ $(expr "$vbid" \% 4) -eq $part ] || continue
-        seenVbuckets=$((seenVbuckets + 1))
-        file="$bucket-$part.mb"
-        "$sqlite" "$file" "SELECT k FROM kv_$vbid"
+(
+    for part in 0 1 2 3; do
+        # Process each bucket-{0,1,2,3}.mb file in parallel
+        (
+            for vbid in $active; do
+                [ $(expr "$vbid" \% 4) -eq $part ] || continue
+                file="$bucket-$part.mb"
+                "$sqlite" "$file" "SELECT k FROM kv_$vbid"
+            done
+        ) &
     done
-done | while read k; do
-    seenItems=$((seenItems + 1))
-    match=0
-    [ -n "$filter" ] && $(echo "$k" | egrep "$filter" > /dev/null) && match=1
-    if [ $match -eq 1 ]; then
-        matchedItems=$((matchedItems + 1))
-        echo "$k"
-    fi
-    if [ $(expr $seenItems % 1000) -eq 0 ]; then
-        printf "matched %d / seen %d\n" "$matchedItems" "$seenItems" >&2
-        #sleep 10
-    fi
-done
+    wait
+) | (
+    while read k; do
+        seenItems=$((seenItems + 1))
+        match=0
+        if [ -n "$filter" ]; then
+            $(echo "$k" | egrep "$filter" > /dev/null) && match=1
+        else
+            match=1
+        fi
+
+        if [ $match -eq 1 ]; then
+            matchedItems=$((matchedItems + 1))
+            echo "$k"
+        fi
+        if [ $(expr $seenItems % 1000) -eq 0 ]; then
+            printf "matched %d / seen %d\n" "$matchedItems" "$seenItems" >&2
+            #sleep 10
+        fi
+    done
+    printf "matched %d / seen %d\n" "$matchedItems" "$seenItems" >&2
+    note "Done (matched $matchedItems keys)"
+)
