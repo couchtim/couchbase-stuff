481e7fe9780ec9a1bd6d48040f11e514ca615c9e
Tim Smith <tim@couchbase.com>
2012-09-29 23:31:13 -0700

    rm-keys: Cluster/bucket/password opts, exit status
    Specify the cluster, bucket name and bucket password.  The cluster
    is required, instead of assuming localhost. The bucket is also
    required, instead of assuming default.
    
    Don't exit(0) if there are errors. Report error count.

--- rm-keys.py
+++ rm-keys.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
-import sys
 import fileinput
+from optparse import OptionParser
+import sys
 
 try:
     import mc_bin_client
@@ -13,16 +14,42 @@ For example, this may work for most Unix users:
 """
     raise
 
-db = mc_bin_client.MemcachedClient('10.4.2.14', 11211)
+parser = OptionParser(usage = "Usage: %prog [options]")
+# NB: required=True param breaks %prog --help
+parser.add_option("-c", "--cluster", metavar="HOST:PORT")
+parser.add_option("-b", "--bucket", metavar="BUCKET_NAME")
+parser.add_option("-p", "--password", default="", metavar="BUCKET_PASSWORD")
+
+(options, args) = parser.parse_args()
+
+for opt in ["cluster", "bucket"]:
+    if getattr(options, opt) is None:
+        sys.stderr.write("Missing required --%s option\n" % (opt,))
+        parser.print_help()
+        sys.exit(1)
+
+host, port = options.cluster.split(':')
+port = int(port)
+
+client = mc_bin_client.MemcachedClient(host, port)
+client.sasl_auth_plain(options.bucket, options.password)
 
 count = 0
+errors = 0
 
-for key in fileinput.input():
+for key in fileinput.input(args):
     key = key.rstrip('\n')
     try:
-        db.delete(key)
+        client.delete(key)
         count += 1
     except mc_bin_client.MemcachedError, e:
+        errors += 1
         print "DELETE ERROR '%s': %s" % (key, e)
 
 print "Done (deleted %d keys)" % (count,)
+
+if errors > 0:
+    print "ERRORS: ", errors
+    sys.exit(2)
+
+sys.exit(0)
